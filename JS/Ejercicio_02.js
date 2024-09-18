7eb39659690e93de35fb02189fd065b94acb4cdd
Jennifer Bautista Barrios <230317@utxicotepec.edu.mx>
2024-09-18 01:33:30 -0600

    Practica 4
    Agregación de String y BigInt al Ejercicio 02

--- JS/Ejercicio_02.js
+++ JS/Ejercicio_02.js
@@ -62,3 +62,55 @@ console.log(`Tu nuevo saldo es de: ${saldo_cuenta}`);
 
 monto_transaccion=1500;
 console.log(`Tu abono de: ${monto_transaccion} ha sido recibido, tu nuevo saldo es de: ${(monto_transaccion+saldo_cuenta)}`);
+
+// 4. Strings (Cadenas de texto)
+console.warn("--- Tipo de dato: Strings o Cadenas de texto");
+const alumno = "Jennifer Bautista Barrios";
+let producto = 'Monitor de 20 pulgadas';
+console.log(`El producto es un ${producto}`);
+console.log(`El nombre del alumno es ${alumno}, que es un tipo de dato ${typeof(producto)}`);
+
+//Manipulando los STRINGS
+console.log(`Más adelante podremos transformar el contenido de los STRINGS usando funciones especificas como convertir a mayúsculas: ${alumno} => ${alumno.toUpperCase()}`);
+console.log(`O en su defecto a minúsculas: ${producto} => ${producto} => ${producto.toUpperCase()}`);
+
+
+
+// 5. BigInt (Enteros de Dimensiones Amplias) - Generalmente utilizado en sistemas matemáticos, físicos o espaciales donde se requiere de una excelente precisión
+console.warn("--- Tipo de dato: BigInt");
+const numeroGrande1 = 1234567890;
+const numeroGrande2 = 12345678901234567890;
+const numeroGrande3 = 123456789012345678901234567890;
+const numeroGrande4 = 1234567890123456789012345678901234567890;
+
+console.log(`El primer experimento de un número grande es: ${numeroGrande1}, que si es soportado por NUMBER, y su tipo de dato es: ${typeof(numeroGrande1)}`);
+console.log(`El primer experimento de un número grande es: ${numeroGrande2}, que si es soportado por NUMBER, y su tipo de dato es: ${typeof(numeroGrande2)}`);
+console.log(`El primer experimento de un número grande es: ${numeroGrande3}, que si es soportado por NUMBER, y su tipo de dato es: ${typeof(numeroGrande3)}`);
+console.log(`El primer experimento de un número grande es: ${numeroGrande4}, que no es soportado por NUMBER, y su tipo de dato es: ${typeof(numeroGrande4)}`);
+const numeroGrande = BigInt(91199191991931237189382178919199191); //leer numeros enteros muy grandes
+console.log(typeof numeroGrande);
+const numero = 10;
+const numero2 = 20;
+console.log(numero + Number(numeroGrande)); //leer numeros enteros a conversion
+
+// const numero = "30";
+// const numero2 = 30;
+// console.log(typeof String(numero2));
+// console.log(typeof Number(numero));
+
+
+
+// 6. Symbol
+console.warn("--- Tipo de dato: Symbol");
+const primerSymbol = Symbol(30);
+const segundoSymbol = Symbol(30);
+console.log(primerSymbol === segundoSymbol);
+console.log(primerSymbol.valueOf());
+console.log(segundoSymbol.valueOf());
+
+
+
+// 7. Null
+console.warn("--- Tipo de dato: Null");
+const descuento = null;
+console.log(typeof descuento);
